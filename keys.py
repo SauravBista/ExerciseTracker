a7b05f598facf945e4a2e0002b0d867036461436
Saurav Bista <sauravbista10@gmail.com>
2024-08-01 08:25:42 +0545

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,4 @@
+
+NUTRITIONIX_APP_ID= "" #Your key from Nutritionix app
+NUTRITIONIX_APP_KEY="" #Your Nutritionx app key
+Basic_Authorization= "" # Your Basic Authorization key example -> "Basic djafkadhfkdjakfhkfn"
